97aa746349cba99f4fdd9992f74991efd7167847
Paul Fothergill <paul@Pauls-iMac-Pro.local>
2020-12-10 15:38:07 -0600

    Working on it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,103 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './index.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight, faChevronLeft, faCircle, faCheckCircle, faPlus } from '@fortawesome/free-solid-svg-icons';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+const App = () => {
+	// HINT: each "item" in our list names a name,
+	// a boolean to tell if its been completed, and a quantity
+	const [items, setItems] = useState([]);
+
+	const [firstNameValue, setfirstNameValue] = useState('');
+	const [lastNameValue, setLastNameValue] = useState('')
+	const [emailValue, setEmailValue] = useState('')
+	const [noteValue, setNoteValue] = useState('')
+	const [totalItemCount, setTotalItemCount] = useState(6);
+
+	const handleAddButtonClick = () => {
+		const newItem = {
+			firstName: firstNameValue,
+			lastName: lastNameValue,
+			email: emailValue,
+			note: noteValue,
+			isSelected: false,
+		};
+
+		const newItems = [...items, newItem];
+
+		setItems(newItems);
+		setfirstNameValue('');
+		setLastNameValue('');
+		setEmailValue('');
+		setNoteValue('');
+	};
+
+	const toggleComplete = (index) => {
+		const newItems = [...items];
+
+		newItems[index].isSelected = !newItems[index].isSelected;
+
+		setItems(newItems);
+	};
+
+	return (
+		<div class="mainView">
+			<div class="contentView">
+				<div class="title">
+					<h1>Add Users</h1>
+				</div>
+				<div class="column">
+					<form>
+						<label>
+							First Name
+							<input value={firstNameValue} onChange={(event) => setfirstNameValue(event.target.value)} placeholder='First Name...' />
+						</label>
+
+						<label>
+							Last Name
+							<input value={lastNameValue} onChange={(event) => setLastNameValue(event.target.value)} placeholder='Last Name...' />
+						</label>
+
+						<label>
+							Email
+							<input value={emailValue} onChange={(event) => setEmailValue(event.target.value)} placeholder='Email...' />
+						</label>
+
+						<label>
+							Note
+							<input value={noteValue} onChange={(event) => setNoteValue(event.target.value)} placeholder='Add a note...' />
+						</label>
+						<FontAwesomeIcon icon={faPlus} onClick={() => handleAddButtonClick()} />
+					</form>
+				</div>
+				<div class="column">
+					<div>
+						{items.map((item, index) => (
+							
+							<div className='item-container'>
+								<div className='item-name' onClick={() => toggleComplete(index)}>
+									{item.isSelected ? (
+										<>
+											<FontAwesomeIcon icon={faCheckCircle} />
+											<span>{'  ' + item.firstName + ' ' + item.lastName + ' | ' + item.email + ' | ' + item.note}</span>
+										</>
+									) : (
+										<>
+											<ul>
+												<span>{item.firstName + ' ' + item.lastName + ' | ' + item.email + ' | ' + item.note}</span>
+												<button type="submit">Remove</button>
+											</ul>
+											
+										</>
+									)}
+								</div>
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,87 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-
-class NameForm extends React.Component {
-  
-  constructor(props) {
-    super(props);
-    this.state = {firstName: ''};
-    this.state = {lastName: ''};
-    this.state = {email: ''};
-    this.state = {note: ''};
-
-    this.handleFirstChange = this.handleFirstChange.bind(this);
-    this.handleLastChange = this.handleLastChange.bind(this);
-    this.handleEmailChange = this.handleEmailChange.bind(this);
-    this.handleNoteChange = this.handleNoteChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleFirstChange(event) {
-    this.setState({firstName: event.target.value});
-  }
-
-  handleLastChange(event) {
-      this.setState({lastName: event.target.value});
-  }
-
-  handleEmailChange(event) {
-    this.setState({email: event.target.value});
-  }
-
-  handleNoteChange(event) {
-    this.setState({note: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('\nFirst Name: ' + this.state.firstName + 
-          '\nLast Name: ' + this.state.lastName +
-          '\nEmail: ' + this.state.email +
-          '\nNote: ' + this.state.note);
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-      <>
-        <h1 style={{textAlign: "center"}}>Add Users</h1>
-        <div class="column">
-          <form onSubmit={this.handleSubmit}>
-            <label>
-              First Name
-              <input type="text" value={this.state.value} onChange={this.handleFirstChange} />
-            </label>
-            <label>
-              Last Name
-              <input type="text" value={this.state.value} onChange={this.handleLastChange} />
-            </label>
-            <label>
-              Email
-              <input type="text" value={this.state.value} onChange={this.handleEmailChange} />
-            </label>
-            <label>
-              Note
-              <input type="text" value={this.state.value} onChange={this.handleNoteChange} />
-            </label>
-            <input type="submit" value="+ Add User" />
-          </form>
-        </div>
-        <div class="column">
-          <label></label>
-        </div>
-
-        
-      </>
-    );
-  }
-}
-
 ReactDOM.render(
   <React.StrictMode>
-    <NameForm />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
